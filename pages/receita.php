557d3253809e927e0a9e0d78fb0afff4f93c6797
kalebepeixinho <kalebepeixinho@gmail.com>
2024-10-31 08:01:29 -0300

    Pagina para gerar receitas

--- pages/receita.php
+++ pages/receita.php
@@ -1,131 +1,207 @@
+<?php
+
+    session_start();
+
+    if((!isset($_SESSION['email']) === TRUE) and (!isset($_SESSION['senha']) === TRUE)){
+        session_unset();
+        session_destroy();
+        echo "
+            <script>
+                window.location.href = 'login.php';
+            </script>
+        ";
+    }
+
+    $id_user = $_SESSION['id_user'];
+    $nome = $_SESSION['nome'];
+    $sobrenome = $_SESSION['sobrenome'];
+    $email = $_SESSION['email'];
+?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/global.css">
-    <link rel="stylesheet" href="../assets/css/style_user.css">
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>RecipeGenie - Receitas</title>
 
-    <style>
-        body{
-            display: flex;
-            justify-content: center;
-            padding: 50px;
-        }
+        <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/css/global.css">
+        <link rel="stylesheet" href="../assets/css/style_user.css">
 
-        .div_container{
-            width: 50%;
-        }
+        <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon">
+    </head>
+    <body class="container-fluid" onload="buscar_receitas(<?php echo $id_user?>)">
 
-        .section_recipes{
-            padding: 50px;
-        }
-
-        .title_recipe{
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .title_recipe button{
-            height: 20px;
-        }
-
-        .info_recipes ul, .ingredientes ul, .modo_de_preparo ul{
-            list-style: none;
-        }
-    </style>
-</head>
-<body>
-    <div class="div_container">
-        <section class="section_recipes">
-            <div class="receita">
-                <div class="title_recipe">
-                    <h1>Nome Receita</h1>
-                    <button>voltar</button>
-                </div>
-                <div class="info_recipes">
-                    <ul>
-                        <li>Categoria: Salgado</li>
-                        <li>Proções: 2 pessoas</li>
-                        <li>Tempo de preparo: 20 minutos</li>
-                    </ul>
-                </div>
-                <div class="ingredientes">
-                    <h3>Ingredientes:</h3>
-                    <ul>
-                        <li>a</li>
-                        <li>b</li>
-                        <li>c</li>
-                    </ul>
-                </div>
-                <div class="modo_de_preparo">
-                    <h3>Modo de preparo:</h3>
-                    <ul>
-                        <li>Etapa 1: </li>
-                        <li>Etapa 2: </li>
-                        <li>Etapa 3: </li>
-                        <li>Etapa 4: </li>
-                    </ul>
-                </div>
-                <div class="tabela_nutricional">
-                    <h3>Tabela nutricional:</h3>
-                    <table border="1">
-                        <caption>Informação Nutricional (Porção: 100g)</caption>
-                        <thead>
-                            <tr>
-                                <th>Item</th>
-                                <th>Quantidade</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Valor energético</td>
-                                <td>170 kcal</td>
-                            </tr>
-                            <tr>
-                                <td>Carboidratos</td>
-                                <td>28g</td>
-                            </tr>
-                            <tr>
-                                <td>Açucares totais</td>
-                                <td>20g</td>
-                            </tr>
-                            <tr>
-                                <td>Açucares adicionados</td>
-                                <td>20g</td>
-                            </tr>
-                            <tr>
-                                <td>Proteínas</td>
-                                <td>3g</td>
-                            </tr>
-                            <tr>
-                                <td>Gorduras totais</td>
-                                <td>6g</td>
-                            </tr>
-                            <tr>
-                                <td>Gorduras saturadas</td>
-                                <td>4g</td>
-                            </tr>
-                            <tr>
-                                <td>Gorduras trans</td>
-                                <td>0g</td>
-                            </tr>
-                            <tr>
-                                <td>Fibra alimentar</td>
-                                <td>1g</td>
-                            </tr>
-                            <tr>
-                                <td>Sódio</td>
-                                <td>40mg</td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel"></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" id="modalMessage"></div>
                 </div>
             </div>
-        </section>
-    </div>
-</body>
+        </div>
+
+        <nav class="navbar navbar-expand-lg nav-edit fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand logo" href="#">
+                    <img src="../assets/images/favicon.png" width="40px" alt="">
+                    Recipe Genie
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse  horizontal-edit" id="navbarNav">
+                    <!-- NavBar horizontal -->
+                    <ul class="navbar-nav ms-auto d-none d-lg-flex">
+                        <form>
+                            <div class="input_container side-bar-pesq">
+                                <input type="text" name="pesquisa_value" id="pesquisa_value" placeholder="Pesquisar por nome">
+                                <button type="button" onclick="buscar_filtro(<?php echo $id_user?>, 'pesquisa', '', 0)"><img src="../assets/images/pesq.png" width="25px" alt=""></button>
+                            </div>
+                        </form>
+                    </ul>
+                    <!-- NavBar Vertical expandida-->
+                    <ul class="navbar-nav d-lg-none nav-vertical-edit">
+                        <div class="side-bar-container">
+                            <button class="btn botao sidebar-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                <span class="btn-text">Categorias</span>
+                                <img class="btn-icon" src="../assets/images/seta_baixo.png" width="20px" alt="">
+                            </button>
+                            <div class="collapse collapse-edit" id="collapseExample">
+                                <div class="card card-body filtro-card">
+                                    <input type="button" value="Todas" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'todas', 0)">
+                                    <input type="button" value="Salgados" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'salgado', 0)">
+                                    <input type="button" value="Doces" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'doce', 0)">
+                                    <input type="button" value="Fitness" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'fitness', 0)">
+                                </div>
+                            </div>
+                            <button class="btn botao sidebar-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
+                                <span class="btn-text">Tempo de preparo</span>
+                                <img class="btn-icon" src="../assets/images/seta_baixo.png" width="20px" alt="">
+                            </button>
+                            <div class="collapse collapse-edit" id="collapseExample1">
+                                <div class="card card-body filtro-card">
+                                    <input type="button" value="-15 minutos" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'tempo', 1, 0)">
+                                    <input type="button" value="15 - 30 minutos" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'tempo', 2, 0)">
+                                    <input type="button" value="+30m minutos" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'tempo', 3, 0)">
+                                </div>
+                            </div>
+                            <button class="btn botao sidebar-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
+                                <span class="btn-text">Avaliação</span>
+                                <img class="btn-icon" src="../assets/images/seta_baixo.png" alt="">
+                            </button>
+                            <div class="collapse collapse-edit" id="collapseExample2">
+                                <div class="card card-body filtro-card">
+                                    <input type="button" value="Maior" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'avaliacao', 'maior', 0)">
+                                    <input type="button" value="Menor" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'avaliacao', 'menor', 0)">
+                                </div>
+                            </div>
+                            <hr>
+                            <a class="nav-btn sidebar-nav-btn">
+                                <input type="button" value="Gerar Receita ⬈">
+                            </a>
+                            <form action="../config/logout.php">
+                                <a class="nav-btn sidebar-nav-btn">
+                                    <input type="submit" value="Sair" style="margin-top: 3px;">
+                                </a>
+                            </form>
+                            <hr>
+                            <form>
+                                <div class="input_container side-bar-pesq">
+                                    <input type="text" name="pesquisa_value1" id="pesquisa_value1" placeholder="Pesquisar por nome">
+                                    <button type="button" onclick="buscar_filtro(<?php echo $id_user?>, 'pesquisa', '', 1)"><img src="../assets/images/pesq.png" width="25px" alt=""></button>
+                                </div>
+                            </form>
+                            <div class="footer-nav-expand">
+                                <div class="left-section">
+                                    <a href="#">Sobre Nós</a>
+                                    <a href="#">Feedback</a>
+                                    <span class="copyright">© 2024 RecipeGenie</span>
+                                </div>
+                                <h1 class="h1">RecipeGenie</h1>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="sidebar">
+                        <ul class="side-list">
+                        <button class="btn botao sidebar-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                <span class="btn-text">Categorias</span>
+                                <img class="btn-icon" src="../assets/images/seta_baixo.png" width="20px" alt="">
+                            </button>
+                            <div class="collapse collapse-edit" id="collapseExample">
+                                <div class="card card-body filtro-card">
+                                    <input type="button" value="Todas" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'todas', 0)">
+                                    <input type="button" value="Salgados" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'salgado', 0)">
+                                    <input type="button" value="Doces" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'doce', 0)">
+                                    <input type="button" value="Fitness" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'categoria', 'fitness', 0)">
+                                </div>
+                            </div>
+                            <button class="btn botao sidebar-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
+                                <span class="btn-text">Tempo de preparo</span>
+                                <img class="btn-icon" src="../assets/images/seta_baixo.png" width="20px" alt="">
+                            </button>
+                            <div class="collapse collapse-edit" id="collapseExample1">
+                                <div class="card card-body filtro-card">
+                                    <input type="button" value="-15 minutos" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'tempo', 1, 0)">
+                                    <input type="button" value="15 - 30 minutos" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'tempo', 2, 0)">
+                                    <input type="button" value="+30m minutos" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'tempo', 3, 0)">
+                                </div>
+                            </div>
+                            <button class="btn botao sidebar-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
+                                <span class="btn-text">Avaliação</span>
+                                <img class="btn-icon" src="../assets/images/seta_baixo.png" alt="">
+                            </button>
+                            <div class="collapse collapse-edit" id="collapseExample2">
+                                <div class="card card-body filtro-card">
+                                    <input type="button" value="Maior" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'avaliacao', 'maior', 0)">
+                                    <input type="button" value="Menor" class="collapse_button" onclick="buscar_filtro(<?php echo $id_user?>, 'avaliacao', 'menor', 0)">
+                                </div>
+                            </div>
+                            <hr>
+                            <a class="nav-btn sidebar-nav-btn">
+                                <input type="button" value="Gerar Receita ⬈">
+                            </a>
+                            <form action="../config/logout.php">
+                                <a class="nav-btn sidebar-nav-btn">
+                                    <input type="submit" value="Sair" style="margin-top: 3px;">
+                                </a>
+                            </form>
+                            <div class="footer-nav">
+                                <div class="left-section">
+                                    <a href="#">Sobre Nós</a>
+                                    <a href="#">Feedback</a>
+                                    <span class="copyright">© 2024 RecipeGenie</span>
+                                </div>
+                                <h1 class="h1">RecipeGenie</h1>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-9 main-container">
+                    <div class="container_title">
+                        
+                    </div>
+                    <section>
+                        
+                    </section>
+                </div>
+            </div>
+        </div>
+
+
+        <script src="../assets/js/jquery-3.7.1.min.js"></script>
+        <script src="../assets/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/js/script_user.js"></script>
+    </body>
 </html>
