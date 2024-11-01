9940678a899d96d8005d7978f4ad310a623d45f1
Andrey Lopes Cavalcante Mendes <129072446+AndreyCavalcante@users.noreply.github.com>
2024-10-31 23:26:41 -0300

    adicionando função de gerar receitas

--- config/gerar_receitas.php
+++ config/gerar_receitas.php
@@ -1,3 +1,98 @@
 <?php
 
-    
+    require __DIR__ . '/../vendor/autoload.php';
+
+    use Dotenv\Dotenv;
+    use GuzzleHttp\Client;
+    use GuzzleHttp\Exception\RequestException;
+
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+
+    $ingredientes = ['ovo', 'leite', 'pao', 'alface'];
+
+    $apiKey = $_ENV['API_KEY'] ?? 'default_value';
+
+    $modelo = json_encode([
+        'receita' => [
+            [
+                'nome' => 'Nome da receita',
+                'categoria' => 'doce, salgado ou fitness',
+                'porcoes' => 'x (subistituir por um numero inteiro)',
+                'tempo_de_preparo' => 'x minutos',
+                'ingredientes' => [
+                    ['nome'=> 'ingrediente_1', 'quant' => 1, 'uni_medida' => 'unidade'],
+                    ['nome'=> 'ingrediente_2', 'quant' => 300, 'uni_medida' => 'gramas']
+                ],
+                'modo_preparo' => [
+                    ['etapa' => 'Primeira etapa'],
+                    ['etapa' => 'Segunda etapa']
+                ],
+                'tabela_nutri' => [
+                    'porcao' => '100 gramas',
+                    'informacoes' => [
+                        ['item' => 'Valor energético', 'quant' => '140 kcal'],
+                        ['item' => 'Carboidrato', 'quant' => '10g'],
+                        ['item' => 'Açucares totais', 'quant' => '10g'],
+                        ['item' => 'Açucares adicionados', 'quant' => '10g'],
+                        ['item' => 'Proteínas', 'quant' => '10g'],
+                        ['item' => 'Gordura totais', 'quant' => '10g'],
+                        ['item' => 'Gorduras saturadas', 'quant' => '10g'],
+                        ['item' => 'Gorduras trans', 'quant' => '10g'],
+                        ['item' => 'Fibra alimentar', 'quant' => '10g'],
+                        ['item' => 'Sódio', 'quant' => '10g']
+                    ]
+                ]
+            ]
+        ]
+    ]);
+
+    $erro = "";
+
+    function gerar_receitas($ingredientes, $apiKey, $modelo){
+        $client = new Client([
+            'base_uri' => 'https://api.openai.com/v1/',
+            'headers' => [
+                'Authorization' => 'Bearer ' . $apiKey,
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+
+        $prompt = "Crie uma receita deliciosa utilizando os seguintes ingredientes: " . implode(', ', $ingredientes) . ". Gere a receita em um JSON estruturado seguindo este modelo:\n" . $modelo . "\nResponda apenas com o JSON, sem texto adicional.";
+
+        try{
+            $response = $client->post('chat/completions', [
+                'json' => [
+                    'model' => 'gpt-4',
+                    'messages' => [
+                        ['role' => 'system', 'content' => 'Você é um assistente de cozinhas que gera receitas simples utilizando apenas os ingredientes disponíveis na cozinha.'],
+                        ['role' => 'user', 'content' => $prompt],
+                    ],
+                    'max_tokens' => 1000,
+                    'temperature' => 0.5,
+                ],
+            ]);
+
+            $data = json_decode($response->getBody(), true);
+
+            if (isset($data['choices'][0]['message']['content'])) {
+                $receitaJson = $data['choices'][0]['message']['content'];
+
+                $receita = json_decode($receitaJson, true);
+
+                if (json_last_error() === JSON_ERROR_NONE) {
+                    echo "<pre>" . print_r($receita, true) . "</pre>";
+                } else {
+                    echo "Erro ao decodificar a resposta da API em JSON.";
+                }
+
+            } else {
+                echo "Nenhuma receita foi gerada.";
+            }
+
+        }catch (RequestException $e){
+            $erro = 'Erro ao gerar a receita: ' . $e->getMessage();
+        }
+    }
+
+    gerar_receitas($ingredientes, $apiKey, $modelo);
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -11,8 +11,13 @@ return array(
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Symfony\\Contracts\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher-contracts'),
     'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src', $vendorDir . '/psr/http-factory/src'),
+    'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
     'Psr\\EventDispatcher\\' => array($vendorDir . '/psr/event-dispatcher/src'),
     'PhpOption\\' => array($vendorDir . '/phpoption/phpoption/src/PhpOption'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
     'GrahamCampbell\\ResultType\\' => array($vendorDir . '/graham-campbell/result-type/src'),
     'Dotenv\\' => array($vendorDir . '/vlucas/phpdotenv/src'),
 );
